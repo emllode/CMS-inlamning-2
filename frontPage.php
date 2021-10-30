3c6f7e6a48859395bcba03e6ff1d81616a39a686
Christopher Bergman <cb.christopherbergman@gmail.com>
2021-10-30 10:58:10 +0200

    Added frontpage section, including header/footer parts and added hero section.

---
+++ frontPage.php
@@ -0,0 +1,24 @@
+<?php
+get_header();
+$getTitle = get_field('get_title');
+$getDescription = get_field('get_description');
+$getImage = get_field('get_image');
+$getLinkRegister = get_field('get_link_register');
+$getLinkLogin = get_field('get_link_login');
+?>
+
+<div class="container">
+    <p class="display-4"><?php echo $getTitle; ?></p>
+    <div class="column">
+        <p><?php echo $getDescription; ?></p>  
+        <img src="<?php echo $getImage; ?>">
+    </div>
+    <div>
+        <a class="link" href="<?php echo $getLinkRegister;?>" class="button" target="_blank">Register</button>
+        <a class="link" href="<?php echo $getLinkLogin;?>" class="button" target="_blank">Login</button>
+    </div>
+</div>
+
+<?php
+get_footer();
+?>
