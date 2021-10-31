e7566b224b7446a7d55552b7cd9b4fe58b209ec3
Christopher <cb.christopherbergman@gmail.com>
2021-10-31 19:59:25 +0100

    smaller changes to make it work with wordpress.

--- functions.php
+++ functions.php
@@ -1,15 +1,14 @@
 <?php
 
-function add_stylesheet()
-{
-    wp_enqueue_style('style', get_stylesheet_uri());
+function addStyleSheets():void {
+	wp_enqueue_style('style', get_stylesheet_uri());
 }
 
 add_action('wp_enqueue_scripts', 'addStyleSheets');
 
 
 register_nav_menus(
-    array(
-        'main-menu' => 'primary',
-    )
-)
+	array(
+		'main-menu' => 'primary',
+	)
+);
